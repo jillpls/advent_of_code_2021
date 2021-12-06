d39b683ed1b3149844029dce747b5c03c8b81816
Jill Please <jillplspls@gmail.com>
2021-12-06 16:58:14 +0100

    06 in Rust

---
+++ rust/src/bin/06/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::fs;
+use std::str::Split;
+use chrono;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let args = &args[1..];
+
+    if args.is_empty() {
+        panic!();
+    }
+
+    let start = chrono::Utc::now();
+
+    let input = fs::read_to_string(&args[0]).unwrap();
+
+    let fish_input : Split<char> = input.split(',');
+    let mut lantern_fish = vec![0; 9];
+
+    for f in fish_input {
+        let fish_idx = f.trim().parse::<usize>().unwrap();
+        lantern_fish[fish_idx] += 1;
+    }
+
+    let mut idx: usize = 0;
+
+    for d in 0..256 {
+        let add_idx = (idx + 7) % 9;
+        lantern_fish[add_idx] += lantern_fish[idx];
+        idx = (idx + 1) % 9;
+    }
+
+    let sum: usize = lantern_fish.iter().sum();
+    let end = chrono::Utc::now();
+    let duration = end-start;
+
+    println!("Total fish: {}", sum);
+    println!("Time elapsed: {} ms", duration.num_nanoseconds().unwrap_or(0) as f64 / 1000000.0);
+}
